b5984e351d4f80d48f3269b064b083772f41e51d
Carlos <carlos.mendoza007@hotmail.com>
2023-04-07 21:47:36 -0500

    se llenó la primera parte de los condicionales

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,27 @@
 ### conditionals
 
-edad1 = 20
+'''edad1 = 20
 edad2 = 21
 
 if edad1 >= edad2:
     print('Sí es mayor!')
 else:
-    print('no, la edad es menor')  ##esto es prueba para el commit           
+    print('no, la edad es menor')  ##esto es prueba para el commit'''   
+
+my_condition = False
+
+if my_condition: #Es lo mismo que if my_condition == True:
+    print('se ejecuta la condición del if')
+
+print('La ejecución continúa')
+
+
+my_condition = 5 * 2
+
+if my_condition == 10: 
+    print('Se ejecuta la segunda condición del segundo if')
+
+if my_condition > 10: 
+    print('Se ejecuta la segunda condición del tercer if')
+
+print('La ejecución continúa')
