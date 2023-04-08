6e98417ade133928486769e57b4b3ef4c386d7ee
Carlos <carlos.mendoza007@hotmail.com>
2023-04-08 18:43:45 -0500

    finalizado loops

--- loops.py
+++ loops.py
@@ -20,3 +20,32 @@ while my_condition < 20:
     print(my_condition)
 
 print('La ejecución continúa')
+
+# For
+
+my_list = [30, 24, 62, 52, 35, 35, 17] 
+
+for element in my_list:
+    print(element)
+
+
+my_tuple = (30, 1.80, 'Carlos', 'Mendoza')
+
+for element in my_tuple:
+    print(element)
+
+my_set = {'Carlos', 'Mendoza', 30}
+for element in my_set:
+    print(element)
+
+my_dict = {'Nombre':'Carlos', 'Apellido':'Mendoza', 'Edad':30, 1:'Python'}
+
+for element in my_dict:
+    print(element)
+    if element == 'Edad':
+        continue
+    print('Se ejecuta')
+else:
+    print('El bucle for para diccionario ha finalizado')
+
+print('La ejecución continúa')
